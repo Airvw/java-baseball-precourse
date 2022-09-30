194782b499d6beee87d31a45dad7a148d4b0e2ea
DoubleK <airvw14@gmail.com>
2022-09-30 23:55:57 +0900

    refactor: use enum for ball status

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -3,15 +3,24 @@ package baseball;
 public class Ball {
     private int number;
     private int position;
+
     public Ball(int number, int postion) {
         this.number = number;
         this.position = postion;
     }
 
 
-    public String getBallStatus(Ball randomBall) {
-        if(number == randomBall.number && position == randomBall.position) return "strike";
-        if(number == randomBall.number) return "ball";
-        return "nothing";
+    public BallStatus getBallStatus(Ball randomBall) {
+        if(number == randomBall.number && position == randomBall.position) return BallStatus.STRIKE;
+        if(number == randomBall.number) return BallStatus.BALL;
+        return BallStatus.NOTHING;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
--- src/test/java/baseball/BallTest.java
+++ src/test/java/baseball/BallTest.java
@@ -21,20 +21,20 @@ public class BallTest {
     @DisplayName("스트라이크 테스트")
     void strikeTest(){
         pickBall = new Ball(9, 0);
-        assertThat(pickBall.getBallStatus(randomBall)).isEqualTo("strike");
+        assertThat(pickBall.getBallStatus(randomBall)).isEqualTo(BallStatus.STRIKE);
     }
 
     @Test
     @DisplayName("볼 테스트")
     void ballTest(){
         pickBall = new Ball(9, 1);
-        assertThat(pickBall.getBallStatus(randomBall)).isEqualTo("ball");
+        assertThat(pickBall.getBallStatus(randomBall)).isEqualTo(BallStatus.BALL);
     }
 
     @Test
     @DisplayName("낫싱 테스트")
     void nothingTest(){
         pickBall = new Ball(8, 0);
-        assertThat(pickBall.getBallStatus(randomBall)).isEqualTo("nothing");
+        assertThat(pickBall.getBallStatus(randomBall)).isEqualTo(BallStatus.NOTHING);
     }
 }
