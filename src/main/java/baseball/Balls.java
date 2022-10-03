2c37f8b1e166c8d0fc1331f8657d18df2bfbfaf2
DoubleK <airvw14@gmail.com>
2022-10-03 22:37:06 +0900

    feat: add function to check ball with List<Ball>
    functions
    - Make randomBallList
    - check playerBall status with randomBallList

--- src/main/java/baseball/Balls.java
+++ src/main/java/baseball/Balls.java
@@ -4,31 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Balls {
+    private RandomBalls randomBalls = new RandomBalls();
+    private List<Ball> randomBallList;
 
-    private int strikeCnt = 0;
-    private int ballCnt = 0;
-    private List<Ball> ballList = new ArrayList<>();
+    public Balls() {
+        this.randomBallList = makeBalls(randomBalls.getRandomNumber());
+    }
 
-    public Balls(String numberStr) {
-        for(int i = 0 ; i < numberStr.length(); i++){
+    public List<Ball> makeBalls(String numberStr){
+        List<Ball> ballList = new ArrayList<>();
+        for(int i = 0; i < numberStr.length(); i++){
             ballList.add(new Ball(numberStr.charAt(i) - '0', i));
         }
-    }
-
-//    public void checkBallStatus(List<Ball> ballList){
-//        for(Ball ball : ballList){
-//            isStike()
-//        }
-//    }
-
-    public int getStrikeCnt() {
-        return strikeCnt;
-    }
-
-    public int getBallCnt() {
-        return ballCnt;
-    }
-    public List<Ball> getBalls() {
         return ballList;
+    }
+
+    public Play checkPlayerBalls(List<Ball> playerBallList){
+        Play play = new Play();
+        playerBallList.stream().map(this::checkBall).forEach(play::checkStatusCnt);
+        return play;
+    }
+    private BallStatus checkBall(Ball playerBall) {
+        return randomBallList.stream().map(randomBall -> randomBall.getBallStatus(playerBall))
+                .filter(BallStatus::isNotNothing)
+                .findFirst()
+                .orElse(BallStatus.NOTHING);
     }
 }
